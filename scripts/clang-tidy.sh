9340c9a603be7cfaeb5effd2c6ccfef63b85b8ae
ClemCliss05 <clissonclement05@gmail.com>
2026-03-11 22:43:05 +0000

    build: linting rules modification and add clang-tidy to CI

--- scripts/clang-tidy.sh
+++ scripts/clang-tidy.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+set -e
+
+echo "Configuring CMake..."
+
+cmake -B build -DCMAKE_TOOLCHAIN_FILE=toolchain/arm-gcc-toolchain.cmake
+
 echo "Running linting with clang-tidy..."
 
-clang-tidy \
-src/*.cpp \
--p build
+run-clang-tidy -p build -quiet
 
 echo "Clang-tidy finished."
