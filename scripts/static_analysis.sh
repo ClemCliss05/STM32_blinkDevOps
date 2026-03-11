8fa53a58922b116d103935dac6f8c30e75ea1368
ClemCliss05 <clissonclement05@gmail.com>
2026-03-11 02:45:21 +0000

    build: make static analysis more strict

--- scripts/static_analysis.sh
+++ scripts/static_analysis.sh
@@ -6,15 +6,15 @@ echo "Running static analysis with cppcheck..."
 
 mkdir -p build
 cppcheck \
---enable=warning,style,performance,portability \
+--enable=all \
 --inconclusive \
 --std=c++17 \
+--force \
 --error-exitcode=1 \
+-Iinclude \
+-Isrc \
 --suppress=missingIncludeSystem \
--I src \
--I include \
 src include
-
-2> build/cppcheck-report.txt | 
+2> build/cppcheck-report.txt
 
 echo "Cppcheck finished. Report saved to cppcheck-report.txt"
