469c905dea24b4f7dc5f75395c20f62d8b32574a
ClemCliss05 <clissonclement05@gmail.com>
2026-03-11 00:41:40 +0000

    scripts: modification to analyse only src and include directories

--- scripts/static_analysis.sh
+++ scripts/static_analysis.sh
@@ -4,13 +4,17 @@ set e
 
 echo "Running static analysis with cppcheck..."
 
+mkdir -p build
 cppcheck \
---enable=all \
+--enable=warning,style,performance,portability \
 --inconclusive \
 --std=c++17 \
--I drivers/CMSIS/Include \
--I drivers/CMSIS/Device/ST/STM32F0xx/Include \
-src \
-2> cppcheck-report.txt
+--error-exitcode=1 \
+--suppress=missingIncludeSystem \
+-I src \
+-I include \
+src include
+
+2> build/cppcheck-report.txt | 
 
 echo "Cppcheck finished. Report saved to cppcheck-report.txt"
