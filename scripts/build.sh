ee71300c916a3aeb0b5d8792f5b387cf47194db7
ClemCliss05 <clissonclement05@gmail.com>
2026-03-11 00:50:20 +0000

    build: add flash option in the build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,39 @@
 
 set -e
 
-cmake -B build \
--DCMAKE_TOOLCHAIN_FILE=toolchain/arm-gcc-toolchain.cmake
+BUILD_DIR=build
+ELF_FILE=$BUILD_DIR/STM32_blinkDevOps.elf
 
-cmake --build build
+build() {
+    cmake -B $BUILD_DIR \
+    -DCMAKE_TOOLCHAIN_FILE=toolchain/arm-gcc-toolchain.cmake
+
+    cmake --build $BUILD_DIR
+}
+
+flash() {
+
+    JLinkExe \
+    -device STM32F072RB \
+    -if SWD \
+    -speed 4000 \
+    -autoconnect 1 \
+    -CommanderScript scripts/flash.jlink
+}
+
+clean() {
+    rm -rf $BUILD_DIR
+}
+
+case "$1" in
+    flash)
+        build
+        flash
+        ;;
+    clean)
+        clean
+        ;;
+    *)
+        build
+        ;;
+esac
